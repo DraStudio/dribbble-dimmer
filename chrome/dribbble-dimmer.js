4af9af9de64db18ab2292c38c7804e006b02d552
Oscar Cortez <ocortez@fullsail.com>
2016-03-22 14:25:03 -0400

    Update jQuery and tried to fix for the new Dribbble

--- chrome/dribbble-dimmer.js
+++ chrome/dribbble-dimmer.js
@@ -1,13 +1,15 @@
 // add UI elements
-$('body').prepend('<div id="dd-mask"></div>');
-$('.single-img').append('<span id="dd-toggle"></span>');
+$('div.overlay-content').prepend('<div id="dd-mask"></div>');
+$('div.the-shot div.single-img').append('<span id="dd-toggle"></span>');
 
 // observe toggle mouseovers
 $('#dd-toggle').hover(
   function(e) {
     $('#dd-mask').fadeIn('fast');
+    $('.shot-overlay').css("background-color", "rgba(0,0,0,0.93)");
   },
   function(e) {
     $('#dd-mask').fadeOut('fast');
+    $('.shot-overlay').css("background-color", "rgba(30,30,30,0.9)");
   }
 );
